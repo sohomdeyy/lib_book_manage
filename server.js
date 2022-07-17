87133595b9adea16b98fca54ba6d8a5d0e147140
sohom dey <sohomjuit@gmail.com>
2022-07-17 23:09:38 +0530

    primary setup

---
+++ server.js
@@ -0,0 +1,32 @@
+
+const express=require('express');
+const expresslayout=require('express-ejs-layouts');
+const indexrouter=require('./routes/index');
+const mongoose=require('mongoose');
+const app=express();
+require('dotenv').config();
+
+mongoose.connect(process.env.DB,{
+    useNewUrlParser:true
+})
+// const db=mongoose.connection;
+// db.on('error',error=>console.log(error));
+// db.once('open',()=>console.log('connected to mongo'));
+
+mongoose
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connection successful"))
+  .catch((err) => console.log(err));
+
+app.set('view engine','ejs');
+app.set('views',__dirname+'/views'); 
+app.set('layout','layouts/layout');
+app.use(expresslayout);
+app.use(express.static('public'));
+
+
+app.use('/',indexrouter); 
+app.listen(process.env.PORT||3000);
