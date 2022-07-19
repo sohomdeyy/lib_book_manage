cd09ad09c127c272a2699cd70092f0c5e83fc949
sohom dey <sohomjuit@gmail.com>
2022-07-19 21:03:24 +0530

    adding author and searching author process is done

--- routes/authors.js
+++ routes/authors.js
@@ -1,33 +1,50 @@
 const express=require('express');
 const router=express.Router();
 const Author=require('../models/author');
+
+
+
+
 //all author
-router.get('/',(req,res)=>{
-    res.render('authors/index');
+router.get('/',async (req,res)=>{
+    let searchoption={};
+    if(req.query.name!=null && req.query.name!==''){
+        searchoption.name=new RegExp(req.query.name,'i');
+    }
+    try{
+   const authors=await Author.find(searchoption);
+    res.render('authors/index',{authors:authors,
+    searchoption:req.query});
+    }
+    catch{
+        res.redirect('/');
+    }
 });
+
+
+
 //new author
 router.get('/new',(req,res)=>{
     res.render('authors/new',{author:new Author()});
 });
+
+
+
+
 //create author
-router.post('/',(req,res)=>{
+router.post('/',async (req,res)=>{
     const author=new Author({
         name:req.body.name,
     })
-    author.save((err,newAuthor)=>{
-        if(err){
-            res.render('authors/new',{
-                author:author,
-                errorMessage:'Error creating creating author'
-            })
-        }
-        else{
-            // res.redirect(`authors/${newAuthor.id}`);
-            res.redirect(`authors`);
-        }
-    }); 
-
-   // res.send(req.body.name );
-    //res.send('hi');
+    try{
+        const newAuthor=await author.save();
+        // res.redirect(`authors/${newAuthor.id}`);
+             res.redirect(`authors`);
+    }catch{
+        res.render('authors/new',{
+                        author:author,
+                        errorMessage:'Error creating creating author'
+                    })
+    }
 });
 module.exports=router;
